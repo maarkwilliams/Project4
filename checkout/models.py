ee3b300710a5524697d5b6e76bb4225c3b5859b5
Mark Williams <maarkk@live.co.uk>
2025-07-28 16:35:01 +0100

    complete additional webhook testing

--- checkout/models.py
+++ checkout/models.py
@@ -22,6 +22,8 @@ class Order(models.Model):
     delivery_cost = models.DecimalField(max_digits=6, decimal_places=2, null=False, default=0)
     order_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
     grand_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
+    original_bag = models.TextField(null=False, blank=False, default='')
+    stripe_pid = models.CharField(max_length=254, null=False, blank=False, default='')
 
     def _generate_order_number(self):
         """
@@ -63,7 +65,7 @@ class OrderLineItem(models.Model):
 
     def save(self, *args, **kwargs):
         """
-        Set the lineitem total and update the order total.
+        Set the lineitem total and update the order total
         """
         self.lineitem_total = self.product.price * self.quantity
         super().save(*args, **kwargs)
