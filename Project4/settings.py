d5db30ebb8a4d3a913639cdadf228975d2e2fc5b
Mark Williams <maarkk@live.co.uk>
2025-07-29 11:07:37 +0100

    deploy aws

--- Project4/settings.py
+++ Project4/settings.py
@@ -16,6 +16,7 @@ if os.path.isfile('env.py'):
     import env
 import dj_database_url
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -56,6 +57,7 @@ INSTALLED_APPS = [
     'profiles',
     'django_countries',
     'crispy_forms',
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -179,8 +181,23 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-if os.path.exists(os.path.join(BASE_DIR, 'Project4', 'env.py')):
-    import Project4.env
+
+if 'USE_AWS' in os.environ:
+    
+    AWS_STORAGE_BUCKET_NAME = 'nothingspecial-project4'
+    AWS_S3_REGION_NAME = 'eu-north-1'
+    AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+
+    STATICFILES_STORAGE = 'custom_storages.StaticStorage'
+    STATICFILES_LOCATION = 'static'
+    DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
+    MEDIAFILES_LOCATION = 'media'
+
+    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
+    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
+
 
 FREE_DELIVERY_THRESHOLD = 50
 STANDARD_DELIVERY_PERCENTAGE = 10
@@ -194,6 +211,3 @@ DEFAULT_FROM_EMAIL = 'nothingspecial@mail.com'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-if os.path.isfile('env.py'):
-    import env
