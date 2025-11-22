af064d14ac08f60795348f440d7cee2e02598897
Mark Williams <maarkk@live.co.uk>
2025-11-22 15:50:13 +0000

    add better validation to checkout/profile forms

--- checkout/forms.py
+++ checkout/forms.py
@@ -1,21 +1,22 @@
 from django import forms
+from django.core.exceptions import ValidationError
 from .models import Order
 
 
 class OrderForm(forms.ModelForm):
     class Meta:
         model = Order
-        fields = ('full_name', 'email', 'phone_number',
-                  'street_address1', 'street_address2',
-                  'town_or_city', 'postcode', 'country',
-                  'county',)
+        fields = (
+            'full_name', 'email', 'phone_number',
+            'street_address1', 'street_address2',
+            'town_or_city', 'postcode', 'country',
+            'county',
+        )
 
     def __init__(self, *args, **kwargs):
-        """
-        Add placeholders and classes, remove auto-generated
-        labels and set autofocus on first field
-        """
+        """Add placeholders, classes and remove auto labels."""
         super().__init__(*args, **kwargs)
+
         placeholders = {
             'full_name': 'Full Name',
             'email': 'Email Address',
@@ -28,12 +29,41 @@ class OrderForm(forms.ModelForm):
         }
 
         self.fields['full_name'].widget.attrs['autofocus'] = True
+
         for field in self.fields:
             if field != 'country':
-                if self.fields[field].required:
-                    placeholder = f'{placeholders[field]} *'
-                else:
-                    placeholder = placeholders[field]
+                placeholder = f"{placeholders[field]} *" \
+                    if self.fields[field].required else placeholders[field]
+
                 self.fields[field].widget.attrs['placeholder'] = placeholder
+
             self.fields[field].widget.attrs['class'] = 'stripe-style-input'
             self.fields[field].label = False
+
+
+    def clean_phone_number(self):
+        phone = self.cleaned_data.get("phone_number")
+        if phone and len(phone.replace(" ", "").replace("-", "")) < 7:
+            raise ValidationError("Phone number seems too short.")
+        return phone
+
+    def clean_postcode(self):
+        postcode = self.cleaned_data.get("postcode")
+        if postcode and len(postcode) < 3:
+            raise ValidationError("Postcode must be at least 3 characters.")
+        return postcode
+
+    def clean(self):
+        """
+        Prevent all address fields being blank
+        """
+        cleaned_data = super().clean()
+
+        addr1 = cleaned_data.get("street_address1")
+        town = cleaned_data.get("town_or_city")
+        country = cleaned_data.get("country")
+
+        if not addr1 or not town or not country:
+            raise ValidationError("Please complete the required address fields.")
+
+        return cleaned_data
--- profiles/forms.py
+++ profiles/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.core.exceptions import ValidationError
 from .models import UserProfile
 
 
@@ -8,11 +9,9 @@ class UserProfileForm(forms.ModelForm):
         exclude = ('user',)
 
     def __init__(self, *args, **kwargs):
-        """
-        Add placeholders and classes, remove auto-generated
-        labels and set autofocus on first field
-        """
+        """Add placeholders + Bootstrap classes."""
         super().__init__(*args, **kwargs)
+
         placeholders = {
             'default_phone_number': 'Phone Number',
             'default_postcode': 'Postal Code',
@@ -25,10 +24,44 @@ class UserProfileForm(forms.ModelForm):
         self.fields['default_phone_number'].widget.attrs['autofocus'] = True
         for field in self.fields:
             if field != 'default_country':
-                if self.fields[field].required:
-                    placeholder = f'{placeholders[field]} *'
-                else:
-                    placeholder = placeholders[field]
+                placeholder = (
+                    f"{placeholders.get(field, '')} *"
+                    if self.fields[field].required else placeholders.get(field, '')
+                )
                 self.fields[field].widget.attrs['placeholder'] = placeholder
-            self.fields[field].widget.attrs['class'] = 'border-black rounded-0 profile-form-input'
+
+            self.fields[field].widget.attrs['class'] = (
+                'border-black rounded-0 profile-form-input'
+            )
             self.fields[field].label = False
+
+
+    def clean_default_phone_number(self):
+        phone = self.cleaned_data.get("default_phone_number")
+        if phone and len(phone.replace(" ", "").replace("-", "")) < 7:
+            raise ValidationError("Phone number is too short.")
+        return phone
+
+    def clean_default_postcode(self):
+        postcode = self.cleaned_data.get("default_postcode")
+        if postcode and len(postcode) < 3:
+            raise ValidationError("Postcode must be at least 3 characters.")
+        return postcode
+
+    def clean(self):
+        """
+        Must provide at least one address field OR leave all blank.
+        """
+        cleaned = super().clean()
+
+        addr1 = cleaned.get("default_street_address1")
+        town = cleaned.get("default_town_or_city")
+        country = cleaned.get("default_country")
+
+        if addr1 or town or country:
+            if not addr1 or not town or not country:
+                raise ValidationError(
+                    "If entering address info, you must complete Street Address 1, Town/City and Country."
+                )
+
+        return cleaned
