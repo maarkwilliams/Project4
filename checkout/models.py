f9429fdb11c6f38cfc92aeccd8a1da2153b2c804
Mark Williams <maarkk@live.co.uk>
2025-11-22 15:43:06 +0000

    add better validation to checkout app

--- checkout/models.py
+++ checkout/models.py
@@ -5,19 +5,38 @@ from django.db.models import Sum
 from django.conf import settings
 
 from django_countries.fields import CountryField
+from django.core.validators import MinValueValidator, RegexValidator
+from django.core.exceptions import ValidationError
 
 from products.models import Product
 from profiles.models import UserProfile
 
 
+phone_validator = RegexValidator(
+    regex=r'^\+?[\d\s\-]{7,15}$',
+    message="Enter a valid phone number (7–15 digits, spaces or dashes allowed)."
+)
+
+postcode_validator = RegexValidator(
+    regex=r'^[A-Za-z0-9\s\-]{3,10}$',
+    message="Enter a valid postcode (3–10 letters/numbers)."
+)
+
+
 class Order(models.Model):
     order_number = models.CharField(max_length=32, null=False, editable=False)
-    user_profile = models.ForeignKey(UserProfile, on_delete=models.SET_NULL,
-                                     null=True, blank=True,
-                                     related_name='orders')
+    user_profile = models.ForeignKey(
+        UserProfile, on_delete=models.SET_NULL,
+        null=True, blank=True,
+        related_name='orders'
+    )
     full_name = models.CharField(max_length=50, null=False, blank=False)
     email = models.EmailField(max_length=254, null=False, blank=False)
-    phone_number = models.CharField(max_length=20, null=False, blank=False)
+
+    phone_number = models.CharField(
+        max_length=20, null=False, blank=False,
+        validators=[phone_validator]
+    )
     country = models.CharField(
         max_length=200,
         null=True,
@@ -25,7 +44,10 @@ class Order(models.Model):
         choices=[('', 'Select Country')] + list(CountryField().choices),
         default=''
     )
-    postcode = models.CharField(max_length=20, null=True, blank=True)
+    postcode = models.CharField(
+        max_length=20, null=True, blank=True,
+        validators=[postcode_validator]
+    )
     town_or_city = models.CharField(max_length=40, null=False, blank=False)
     street_address1 = models.CharField(max_length=80, null=False, blank=False)
     street_address2 = models.CharField(max_length=80, null=True, blank=True)
@@ -58,9 +80,7 @@ class Order(models.Model):
     )
 
     def _generate_order_number(self):
-        """
-        Generate a random, unique order number using UUID
-        """
+        """Generate a random, unique order number using UUID"""
         return uuid.uuid4().hex.upper()
 
     def update_total(self):
@@ -68,19 +88,22 @@ class Order(models.Model):
         Update grand total each time a line item is added,
         accounting for delivery costs.
         """
-        self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))['lineitem_total__sum'] or 0
+        self.order_total = self.lineitems.aggregate(
+            Sum('lineitem_total')
+        )['lineitem_total__sum'] or 0
+
         if self.order_total < settings.FREE_DELIVERY_THRESHOLD:
-            self.delivery_cost = self.order_total * settings.STANDARD_DELIVERY_PERCENTAGE / 100
+            self.delivery_cost = (
+                self.order_total * settings.STANDARD_DELIVERY_PERCENTAGE / 100
+            )
         else:
             self.delivery_cost = 0
+
         self.grand_total = self.order_total + self.delivery_cost
         self.save()
 
     def save(self, *args, **kwargs):
-        """
-        Override the original save method to set the order number
-        if it hasn't been set already.
-        """
+        """Set order number if it hasn't been set already."""
         if not self.order_number:
             self.order_number = self._generate_order_number()
         super().save(*args, **kwargs)
@@ -99,17 +122,33 @@ class OrderLineItem(models.Model):
     product = models.ForeignKey(
         Product,
         null=False, blank=False,
-        on_delete=models.CASCADE)
+        on_delete=models.CASCADE
+    )
     product_size = models.CharField(max_length=2, null=True, blank=True)
-    quantity = models.IntegerField(null=False, blank=False, default=0)
+    quantity = models.IntegerField(
+        null=False,
+        blank=False,
+        default=1,
+        validators=[MinValueValidator(1)]
+    )
     lineitem_total = models.DecimalField(
         max_digits=6, decimal_places=2,
-        null=False, blank=False, editable=False)
+        null=False, blank=False, editable=False
+    )
+
+    def clean(self):
+        """
+        If the product has sizes,
+        enforce that a size is chosen.
+        """
+        if self.product and self.product.has_sizes and not self.product_size:
+            raise ValidationError({
+                "product_size": "Please select a size for this product."
+            })
 
     def save(self, *args, **kwargs):
         """
-        Override the original save method to set the lineitem total
-        and update the order total.
+        Set the lineitem total and update the order total.
         """
         self.lineitem_total = self.product.price * self.quantity
         super().save(*args, **kwargs)
